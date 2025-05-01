20259f1776240d8e502af662853cd3417004f384
Франк Влад <f333vv@gmail.com>
2025-05-01 20:38:32 +0700

    Книги

---
+++ lab6/books.js
@@ -0,0 +1,82 @@
+const books = [
+    {
+        author: 'Михаил Лермонтов',
+        name: 'Герой нашего времени',
+        publicationYear: 1838,
+        price: '750 ₽',
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Николай Гоголь',
+        name: 'Ревизор',
+        publicationYear: 1835,
+        price: '800 ₽',
+        bestseller: false,
+        available: true
+    },
+    {
+        author: 'Алексагдр Пушник',
+        name: 'Евгений Онегин',
+        publicationYear: 1823,
+        price: '1000 ₽',
+        bestseller: false,
+        available: true
+    },
+    {
+        author: 'Фёдор Достоевский',
+        name: 'Преступление и наказание',
+        publicationYear: 1866,
+        price: '850 ₽',
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Лев Толстой',
+        name: 'Война и мир',
+        publicationYear: 1869,
+        price: '1200 ₽',
+        bestseller: true,
+        available: false
+    },
+    {
+        author: 'Михаил Булгаков',
+        name: 'Мастер и Маргарита',
+        publicationYear: 1967,
+        price: '950 ₽',
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Антон Чехов',
+        name: 'Вишнёвый сад',
+        publicationYear: 1904,
+        price: '600 ₽',
+        bestseller: false,
+        available: true
+    },
+    {
+        author: 'Николай Гоголь',
+        name: 'Мёртвые души',
+        publicationYear: 1842,
+        price: '750 ₽',
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Иван Тургенев',
+        name: 'Отцы и дети',
+        publicationYear: 1862,
+        price: '700 ₽',
+        bestseller: false,
+        available: false
+    },
+    {
+        author: 'Александр Солженицын',
+        name: 'Архипелаг ГУЛАГ',
+        publicationYear: 1973,
+        price: '1100 ₽',
+        bestseller: true,
+        available: true
+    }
+];
